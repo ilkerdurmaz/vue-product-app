353d6f60e4d2c8494e5172d71494ac6212742f62
ilkerdurmaz <ilker.prime@gmail.com>
2022-03-22 01:34:04 +0300

    route change for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/my-project/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-product-app/" : "/",
 });
